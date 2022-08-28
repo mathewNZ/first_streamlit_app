cbddcce6e0e3bb5ce81f532b327be5a7e7949a51
mathewNZ <41562099+mathewNZ@users.noreply.github.com>
2022-08-28 16:38:10 +1200

    Update streamlit_app.py
    corrected error

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 stremlit.header('Breakfast Menu')
 stremlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie
-              streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
