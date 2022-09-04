55989c7b8a40fa3e6d3c852d232970535643d383
mathewNZ <41562099+mathewNZ@users.noreply.github.com>
2022-09-04 13:05:19 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,14 +28,9 @@ streamlit.dataframe(fruits_to_show)
 
 
 steamlit.header('Fruityvice Fruit Advice!')
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-else :
-    fruityvice_response = requests.get(
-        "https://fruityvice.com/api/fruit/" + fruit_choice)
+try:fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:streamlit.error("Please select a fruit to get information.")
+else :fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-except URLerror a e:
-    streamlit.error()
+except URLerror a e:streamlit.error()
