1a1351b151169753489e898f5c3b7ca1f4c0b74c
mathewNZ <41562099+mathewNZ@users.noreply.github.com>
2022-09-04 13:20:16 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,5 +35,5 @@ try:
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
   
-except URLerror a e:
-    streamlit.error()
+    except URLerror a e:
+      streamlit.error()
